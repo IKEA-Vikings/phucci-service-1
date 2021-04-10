010db7a6ac84f75885e4bc57e6722e971642c090
phucciTr <miabam26@gmail.com>
2021-04-09 17:31:25 -0700

    Edit route

--- client/lib/request.js
+++ client/lib/request.js
@@ -6,7 +6,7 @@ const request = {
     productId = !productId ? 1 : productId;
 
     $.ajax({
-      url: `http://127.0.0.1:3004/images/org/${productId}`,
+      url: `/images/org/${productId}`,
       type: 'GET',
       success: successCb,
       error: () => console.log('Failed to get regular size images for id: ', id)
